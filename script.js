e26e6f964a321e9760351323359da5976b3204df
monika <monikadhakad96@gmail.com>
2026-03-14 15:13:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const songs = [
+  { title: "Song One", src: "songs/song1.mp3" },
+  { title: "Song Two", src: "songs/song2.mp3" }
+]
+
+let currentSong = 0
+const audio = document.getElementById("audio")
+const title = document.getElementById("title")
+const progress = document.getElementById("progress")
+
+loadSong(currentSong)
+
+function loadSong(index) {
+  title.innerText = songs[index].title
+  audio.src = songs[index].src
+}
+
+function playPause() {
+  if (audio.paused) {
+    audio.play()
+  } else {
+    audio.pause()
+  }
+}
+
+function nextSong() {
+  currentSong = (currentSong + 1) % songs.length
+  loadSong(currentSong)
+  audio.play()
+}
+
+function prevSong() {
+  currentSong = (currentSong - 1 + songs.length) % songs.length
+  loadSong(currentSong)
+  audio.play()
+}
+
+audio.addEventListener("timeupdate", () => {
+  progress.value = (audio.currentTime / audio.duration) * 100
+})
+
+progress.addEventListener("input", () => {
+  audio.currentTime = (progress.value / 100) * audio.duration
+})
